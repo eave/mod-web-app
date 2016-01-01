b27d3991ce1cc32aa9f4e75defe5466737e07bc2
Mario Ponticello <marioPonticello@dockerMac.home>
2016-01-01 16:17:22 -0500

    Create repos and public pages

---
+++ src/pages/public.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavHelper from '../components/nav-helper'
+
+export default React.createClass({
+  render () {
+    return (
+      <div>Public page</div>
+    )
+  }
+})
