c96f8bfaf01ef696ccf083b9b5b0ce414581190a
Mario <mpontice@gmail.com>
2016-02-15 15:47:32 -0800

    Add ability to update labels through to GitHub

--- src/models/label.js
+++ src/models/label.js
@@ -1,4 +1,5 @@
 import Model from 'ampersand-model'
+import xhr from 'xhr'
 import githubMixin from '../helpers/github-mixin'
 
 export default Model.extend(githubMixin, {
@@ -15,5 +16,23 @@ export default Model.extend(githubMixin, {
       type: 'boolean',
       default: false
     }
+  },
+
+  update (attributes) {
+    const oldAttributes = this.getAttributes({props: true, session: false})
+    xhr({
+      url: this.url(),
+      json: attributes,
+      method: 'PATCH',
+      headers: {
+        Authorization: 'token ' + app.me.token
+      }
+    }, (err, req, body) => {
+      if (err) {
+        this.set(oldAttributes)
+        console.error('Something went wrong—check your wifi')
+      }
+    })
+    this.set(attributes)
   }
 })
